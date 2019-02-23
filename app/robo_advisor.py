240e344125b5531ea4172f04447b5f6845ff8f8b
th679 <th679@georgetown.edu>
2019-02-23 18:19:58 -0500

    Run and data dates

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -6,19 +6,27 @@
 
 import requests
 import json
+import datetime
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
+run_date = datetime.datetime.now()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
 
 
 print("-----------------------")
 print("STOCK SYMBOL: AMZN")
+print("RUN AT: " + run_date.strftime("%Y-%m-%d %I:%M:%S %p"))
+print("LATEST DATA FROM: " + last_refreshed)
 
 print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
 print("LATEST CLOSING PRICE: $1,259.19")
+print("RECENT HIGH: ")
+print("RECENT LOW: ")
