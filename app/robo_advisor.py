831657945923e1a5b38c79535179ac19fbf14892
th679 <th679@georgetown.edu>
2019-02-25 00:03:42 -0500

    Cleanup

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,9 +1,5 @@
 # app/robo_advisor.py
 
-# TODO: import some modules and/or packages here
-
-# TODO: write some Python code here to produce the desired functionality...
-
 from dotenv import load_dotenv
 import requests
 import json
